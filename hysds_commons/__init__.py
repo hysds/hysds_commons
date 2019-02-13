3b6ac244f8f90b190c80395dea851ca41790a101
gmanipon <gmanipon@jpl.nasa.gov>
2019-02-13 17:42:27 +0000

    pasteurized

--- hysds_commons/__init__.py
+++ hysds_commons/__init__.py
@@ -1,3 +1,9 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
 __version__ = "0.1.2"
 __description__ = "Common HySDS Functions, Utilities, Etc."
 __url__ = "https://github.jpl.nasa.gov/hysds-org/hysds_commons"
--- hysds_commons/job_iterator.py
+++ hysds_commons/job_iterator.py
@@ -1,4 +1,11 @@
 #!/bin/env python
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from builtins import str
+from future import standard_library
+standard_library.install_aliases()
 import copy
 import json
 import traceback
