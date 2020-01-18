152e3b0db9445e0b651696445f23f5c0be2eb6c0
Dustin Lo <dustinlo.pcc@gmail.com>
2020-01-17 21:31:34 -0800

    Hc 152 container builder (#15)
    * moved cofig/es_template_es7.json to es_template.json
    
    * removed es_types from hysds_commons/*_utils.py
    
    * removed extra argument from hysds_commons/metadata_rest_utils.py
    
    * added _doc argument to metadata rest utils
    
    * added warning log to safe mode in get_by_id
    
    * refactored metadata_rest_utils to use elasticsearchh-py lib
    
    * removed hysds_io_type from job_iterator

--- hysds_commons/job_spec_utils.py
+++ hysds_commons/job_spec_utils.py
@@ -8,7 +8,6 @@ import hysds_commons.metadata_rest_utils
 
 
 JOB_SPEC_INDEX = "job_specs"
-JOB_SPEC_TYPE = "job_spec"
 
 
 def get_job_spec_types(es_url, logger=None):
@@ -20,17 +19,17 @@ def get_job_spec_types(es_url, logger=None):
     return hysds_commons.metadata_rest_utils.get_types(es_url, JOB_SPEC_INDEX, logger=logger)
 
 
-def get_job_specs(es_url, logger=None, job_spec_type=JOB_SPEC_TYPE):
+def get_job_specs(es_url, logger=None):
     '''
     Get all job spec docs from ES
     @param es_url - elastic search URL
     @param job_spec_type - es index type, now _doc type in es7+, preserve for now
     @return: dict representing anonymous object of job specs
     '''
-    return hysds_commons.metadata_rest_utils.get_all(es_url, JOB_SPEC_INDEX, job_spec_type, logger=logger)
+    return hysds_commons.metadata_rest_utils.get_all(es_url, JOB_SPEC_INDEX, logger=logger)
 
 
-def get_job_spec(es_url, ident, logger=None, job_spec_type=JOB_SPEC_TYPE):
+def get_job_spec(es_url, ident, logger=None):
     '''
     Get a job_spec (JSON body)
     @param es_url - elasticsearch URL
@@ -38,25 +37,24 @@ def get_job_spec(es_url, ident, logger=None, job_spec_type=JOB_SPEC_TYPE):
     @param job_spec_type - es index type, now _doc type in es7+, preserve for now
     @return: dict representing anonymous object of HySDS IO
     '''
-    return hysds_commons.metadata_rest_utils.get_by_id(es_url, JOB_SPEC_INDEX, job_spec_type, ident, logger=logger)
+    return hysds_commons.metadata_rest_utils.get_by_id(es_url, JOB_SPEC_INDEX, ident, logger=logger)
 
 
-def add_job_spec(es_url, obj, logger=None, job_spec_type=JOB_SPEC_TYPE):
+def add_job_spec(es_url, obj, logger=None):
     '''
     Ingests a job_spec into ES
     @param es_url - elasticsearch URL
     @param job_spec_type - es index type, now _doc type in es7+, preserve for now
     @param obj - object for ingestion into ES
     '''
-    return hysds_commons.metadata_rest_utils.add_metadata(es_url, JOB_SPEC_INDEX, job_spec_type, obj, logger=logger)
+    return hysds_commons.metadata_rest_utils.add_metadata(es_url, JOB_SPEC_INDEX, obj, logger=logger)
 
 
-def remove_job_spec(es_url, ident, logger=None, job_spec_type=JOB_SPEC_TYPE):
+def remove_job_spec(es_url, ident, logger=None):
     '''
     Remove a container
     @param es_url - elasticsearch URL
     @param job_spec_type - es index type, now _doc type in es7+, preserve for now
     @param ident - id to delete
     '''
-    return hysds_commons.metadata_rest_utils.remove_metadata(es_url, JOB_SPEC_INDEX, job_spec_type, ident,
-                                                             logger=logger)
+    return hysds_commons.metadata_rest_utils.remove_metadata(es_url, JOB_SPEC_INDEX, ident, logger=logger)
