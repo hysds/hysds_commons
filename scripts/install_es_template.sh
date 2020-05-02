aca5cef97a713cc142d753edc2c6dc0ad6bf2c25
DustinKLo <dustinlo.pcc@gmail.com>
2020-05-02 13:15:21 -0700

    removed install_es_template.py and es_template.json (moved to mozart since no longer needed in grq)

--- scripts/install_es_template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BASE_PATH=$(dirname "${BASH_SOURCE}")
-BASE_PATH=$(cd "${BASE_PATH}"; pwd)
-export PYTHONPATH=$BASE_PATH/..:$PYTHONPATH
-
-# install templates for containers, job_specs, and hysds_ios
-$BASE_PATH/install_es_template.py $*
