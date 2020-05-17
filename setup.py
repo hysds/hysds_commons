561c4ce64b8a7653a0e127b5b88a8d33f1c998c9
DustinKLo <dustinlo.pcc@gmail.com>
2020-05-17 11:17:42 -0700

    cleared scroll in Elasticsearch query method after use
    removed redundant logging in get_count
    added elasticsearch-py dependency in setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'elasticsearch>=7.0.0,<8.0.0',
         'requests>=2.7.0',
         'future>=0.17.1',
         "jsonschema>=3.0.1"
