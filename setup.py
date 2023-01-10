f3a95a1309eb573f72d367df2b93dc33e71c8652
Dustin Lo <dustinlo.pcc@gmail.com>
2023-01-09 20:45:48 -0800

    HC-431 - Make hysds-framework conda installable  (#51)
    * added additional metadata to setup.py
    
    added additional jobs to circleci/config.yml
    
    * bump version
    
    * added filter to build-and-deploy
    
    * moved pypi-creds context to publish job only
    
    * using cimg docker image
    
    * added 'apt update' to build job
    
    Co-authored-by: dustinlo <dustin.k.lo@jpl.nasa.gov>

--- setup.py
+++ setup.py
@@ -5,10 +5,18 @@ from __future__ import absolute_import
 from setuptools import setup, find_packages
 import hysds_commons
 
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
 setup(
     name='hysds_commons',
     version=hysds_commons.__version__,
-    long_description=hysds_commons.__description__,
+    description=hysds_commons.__description__,
+    long_description_content_type="text/markdown",
+    long_description=readme(),
     url=hysds_commons.__url__,
     packages=find_packages(),
     include_package_data=True,
@@ -19,5 +27,20 @@ setup(
         'future>=0.17.1',
         "jsonschema>=3.0.1",
         "shapely>=1.8.2"
-    ]
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ],
 )
